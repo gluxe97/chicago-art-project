833b4a953e8f2e3be974f5e1caa47463367f42fe
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-02-15 13:45:33 -0500

    updated index.js to add logic for displaying clicked images to details display. Clearing images list when a new search is done. Image to display when the json data doesn't have on. Alert to display when search text doesn't return any artwork. Removed placeholder text from index.html and put detail display place and year in a div so we can try to display on same line later.

--- index.js
+++ index.js
@@ -1,63 +1,107 @@
 document.addEventListener("DOMContentLoaded", () => {
     //Save needed DOM elements to variables
-    const detailImg = document.getElementById("detail-image");
-    const detailTheme = document.getElementById("theme_titles");
     const searchForm = document.getElementById("artist-search");
     const artwork = document.getElementById("artwork-list");
-
-    //Hardcoding values into our display div.
-    //WILL BE REWRITTEN
-    console.log(detailTheme);
-    detailImg.src = 'https://www.artic.edu/iiif/2/e966799b-97ee-1cc6-bd2f-a94b4b8bb8f9/full/843,/0/default.jpg';
-    detailImg.alt = 'Placeholder Image';
-    detailTheme.textContent = `[
-        "African American artists",
-        "Women artists",
-        "African Diaspora"
-      ]`;
+    const title = document.getElementById("title");
+    const detailImg = document.getElementById("detail-image");
+    const dept = document.getElementById("dept");
+    const artistDisp = document.getElementById("artist_display");
+    const place = document.getElementById("place_of_origin")
+    const date = document.getElementById("date_display");
+    const medium = document.getElementById("medium_display");
+    const themes = document.getElementById("theme_titles");
 
     //Add event listener on search form submit
     searchForm.addEventListener("submit", (e) => {
         e.preventDefault();
-        const artist = e.target.search.value;
+        const artist = e.target.search.value.toLowerCase();
         //fetch to artworks/search endpoint to find id's of artworks by requested artist
         fetch(`https://api.artic.edu/api/v1/artworks/search?q=${artist}&limit=100&fields=id,artist_title`)
         .then((resp) => resp.json())
         .then((json) => {
             let i = 0;
             const artworkIds = [];
+            let jsonArtist;
 
+            //loops through up to 100 id, artist_title elements returned by fetch to check if artist_title is set.
+            //If artist_title is not null, checks if case insensitive text input from search form is contained by
+            //case insensitive artist_title. If contained, adds artwork id to array for next fetch. When ten matches
+            //are found or the end of the json.data is reached, loop ends.
             while ((artworkIds.length < 10) && (i < json.data.length)) {
-                if (json.data[i].artist_title === artist) {
-                    console.log("if=true");
-                    artworkIds.push(json.data[i].id)
+                if (json.data[i].artist_title) {
+                    jsonArtist = json.data[i].artist_title.toLowerCase();
+                    if (jsonArtist.includes(artist)) {
+                        console.log("matching artist:",json.data[i].artist_title);
+                        artworkIds.push(json.data[i].id)
+                    }
                 }
                 i++;
             }
-            fetchArtworks(artworkIds.join());
+            //if the array isn't empty, call the function to fetch the artworks and add them to the DOM
+            if (artworkIds.join() !== "") {
+                fetchArtworks(artworkIds.join());
+                fetchDetails(artworkIds[0]);
+            } else {
+                alert(`The Chicago Museum of Art collection contains no works by ${e.target.search.value}`);
+            }
         })
+        searchForm.reset();
     })
 
-    //fetch to artworks endpoint with list of artwork ids
-    //then display to artwork-list div
+    //Clear previous artworks from artwork-list.
+    //Fetch to artworks endpoint with list of artwork ids
+    //Add fetched artworks to artwork-list div with click event listeners
     function fetchArtworks(idString) {
+        artwork.innerHTML = "";
         fetch(`https://api.artic.edu/api/v1/artworks?ids=${idString}`)
         .then((resp) => resp.json())
         .then((json) => {
             json.data.forEach((art) => {
-                console.log(art.id,art.artist_title);
                 const artDiv = document.createElement('div');
                 const artTitle = document.createElement('p');
                 const artImg = document.createElement('img');
                 
                 artDiv.id = art.id;
-                artImg.src = `https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`
-                artImg.alt = art.alt_text;
+                artDiv.addEventListener("click",() => fetchDetails(artDiv.id));
+                if (art.image_id) {
+                    artImg.src = `https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`
+                    artImg.alt = art.alt_text;
+                } else {
+                    artImg.src = "./images/noImageFound.jpeg";
+                    artImg.alt = 'A man at a painting class has painted his canvas yellow and written "No!". Text on the image reads "No image found for this artwork"';
+                }
                 artTitle.textContent = art.title;
                 artDiv.append(artTitle, artImg);
                 artwork.appendChild(artDiv);
             })
             
+        })
+    }
+
+    //Fetch art by its ID and display additional details to details DOM
+    function fetchDetails(artId) {
+        fetch(`https://api.artic.edu/api/v1/artworks/${artId}`)
+        .then((resp) => resp.json())
+        .then((json) => {
+            const data = json.data;
+            title.textContent = data.title;
+            if (data.image_id) {
+                detailImg.src = `https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg`;
+                detailImg.alt = data.alt_text;
+            } else {
+                detailImg.src = "./images/noImageFound.jpeg";
+                detailImg.alt = 'A man at a painting class has painted his canvas yellow and written "No!". Text on the image reads "No image found for this artwork"';
+            }
+            dept.textContent = `Department: ${data.department_title}, ${data.department_id}`;
+            artistDisp.textContent = data.artist_display;
+            place.textContent = `${data.place_of_origin}, `;
+            date.textContent = data.date_display;
+            medium.textContent = data.medium_display;
+            if(data.theme_titles[0]) {
+                themes.textContent = `Themes: ${data.theme_titles}`;
+            } else {
+                themes.textContent = "";
+            }
         })
     }
 })
